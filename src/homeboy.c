60b6ba289ecc657ebf688d46628a24ae7354d835
Louis <35883445+louist103@users.noreply.github.com>
2021-05-01 12:40:39 -0400

    remove backup and restore and use version in GZ

--- src/homeboy.c
+++ src/homeboy.c
@@ -9,215 +9,176 @@
 #include "sd.h"
 #include "fs.h"
 #include "cpu.h"
-#include "vc.h"
 
 int hb_hid = -1;
-hb_sd_regs_t *homeboy_obj = NULL;
-void *n64_dram = NULL;
-char dram_fn[64];
 
-int homeboy_event(void *regs, int event, void *arg);
+static hb_sd_regs_t hb_sd_regs;
+static void *n64_dram = NULL;
+static char dram_fn[64];
 
-static class_type_t homeboy_class = 
-{
-    "HOMEBOY",
-    sizeof(hb_sd_regs_t),
-    0,
-    homeboy_event
+static memory_domain_t memory = {
+        {0,0,0,0},
+        NULL, -hb_mmreg,
+        lb, lh, lw, ld,
+        sb, sh, sw, sd,
+        unk_0x2C_,
+        hb_mmreg,
+        hb_mmreg | 0xFFFF,
+        0x00000000, 0x0000FFFF,
 };
 
-static void do_write() 
-{
-    homeboy_obj->ready = 0;
-    homeboy_obj->busy = 1;
-
-    if(sdio_write_sectors(homeboy_obj->write_lba, homeboy_obj->block_cnt, (void*)((char*)n64_dram + homeboy_obj->addr)))
-    {
-        homeboy_obj->error = SD_ERROR_SUCCESS;
+static void do_write(){
+    hb_sd_regs.ready = 0;
+    hb_sd_regs.busy = 1;
+    if(sdio_write_sectors(hb_sd_regs.write_lba,hb_sd_regs.block_cnt,(void*)((char*)n64_dram + hb_sd_regs.addr))){
+        hb_sd_regs.error = SD_ERROR_SUCCESS;
+    }else{
+        hb_sd_regs.error = SD_ERROR_INVAL;
     }
-    else
-    {
-        homeboy_obj->error = SD_ERROR_INVAL;
-    }
-
-    homeboy_obj->ready = 1;
-    homeboy_obj->busy = 0;
+    hb_sd_regs.ready = 1;
+    hb_sd_regs.busy = 0;
 }
 
-static void do_read()
-{
-    homeboy_obj->ready = 0;
-    homeboy_obj->busy = 1;
-
-    if(sdio_read_sectors(homeboy_obj->read_lba, homeboy_obj->block_cnt, (void*)((char*)n64_dram + homeboy_obj->addr)))
-    {
-        homeboy_obj->error = SD_ERROR_SUCCESS;    
+static void do_read(){
+    hb_sd_regs.ready = 0;
+    hb_sd_regs.busy = 1;
+    if(sdio_read_sectors(hb_sd_regs.read_lba,hb_sd_regs.block_cnt,(void*)((char*)n64_dram + hb_sd_regs.addr))){
+        hb_sd_regs.error = SD_ERROR_SUCCESS;    
+    }else{
+        hb_sd_regs.error = SD_ERROR_INVAL;
     }
-    else
-    {
-        homeboy_obj->error = SD_ERROR_INVAL;
-    }
-
-    homeboy_obj->ready = 1;
-    homeboy_obj->busy = 0;
+    hb_sd_regs.ready = 1;
+    hb_sd_regs.busy = 0;
 }
 
- static void do_status_update()
- {
-    if(homeboy_obj->reset)
-    {
-        int fd = fs_open(dram_fn, 3);
-        if(fd < 0)
-        {
-            fd = fs_create(dram_fn, 3);
+ static void do_status_update(){
+    if(hb_sd_regs.reset){
+        int fd = fs_open(dram_fn,3);
+        if(fd<0){
+            fd = fs_create(dram_fn,3);
         }
-
-        if(fd >= 0)
-        {
+        if(fd>=0){
             uint32_t dram_params[2];
-            dram_params[0] = homeboy_obj->dram_save;
-            dram_params[1] = homeboy_obj->dram_save_len;
-            fs_write(fd, dram_params, sizeof(dram_params));
-            fs_write(fd, (char*)n64_dram + dram_params[0], dram_params[1]);
+            dram_params[0] = hb_sd_regs.dram_save;
+            dram_params[1] = hb_sd_regs.dram_save_len;
+            fs_write(fd,dram_params,sizeof(dram_params));
+            fs_write(fd,(char*)n64_dram + dram_params[0],dram_params[1]);
             fs_close(fd);
         }
         
         reset_flag = 1;
+        
     }
-
-    if(homeboy_obj->initialize) 
-    {
-        if(sdio_is_initialized())
-        {
+    if(hb_sd_regs.initialize){
+        if(sdio_is_initialized()){
             sdio_stop();
         }
-
-        homeboy_obj->busy = 1;
-        homeboy_obj->ready = 0;
-
-        if(sdio_start())
-        {
-            homeboy_obj->error = SD_ERROR_SUCCESS;
-            homeboy_obj->inserted = sdio_is_inserted();
-            homeboy_obj->sdhc = sdio_is_sdhc();
-            homeboy_obj->ready = 1;
-        } 
-        else
-        {
-            homeboy_obj->error = SD_ERROR_INVAL;
+        hb_sd_regs.busy = 1;
+        hb_sd_regs.ready = 0;
+        if(sdio_start()){
+            hb_sd_regs.error = SD_ERROR_SUCCESS;
+            hb_sd_regs.inserted = sdio_is_inserted();
+            hb_sd_regs.sdhc = sdio_is_sdhc();
+            hb_sd_regs.ready = 1;
+        }else{
+            hb_sd_regs.error = SD_ERROR_INVAL;
         }
-
-        homeboy_obj->busy = 0;
+        hb_sd_regs.busy = 0;
     }
 }
 
-#if IS_OOT
-#define ADDR_OFFSET 0x08050000
-#else 
-#define ADDR_OFFSET 0x100A0000
-#endif
-
-bool lb(hb_sd_regs_t *hb_regs, uint32_t addr, uint8_t *dest)
-{
-    addr -= ADDR_OFFSET;
-    *dest = (uint8_t)hb_regs->regs[addr >> 2];
-
-    return true;
+uint8_t lb(void* callback, uint32_t addr, uint8_t* dest){
+    *dest = (uint8_t)hb_sd_regs.regs[addr>>2];
+    return 1;
 }
-
-bool lh(hb_sd_regs_t *hb_regs, uint32_t addr, uint16_t *dest)
-{
-    addr -= ADDR_OFFSET;
-    *dest = (uint16_t)hb_regs->regs[addr >> 2];
-
-    return true;
+uint8_t lh(void* callback, uint32_t addr, uint16_t* dest){
+    *dest = (uint16_t)hb_sd_regs.regs[addr>>2];
+    return 1;
 }
-
-bool lw(hb_sd_regs_t *hb_regs, uint32_t addr, uint32_t *dest)
-{
-    addr -= ADDR_OFFSET;
-
-    if(addr == 0x024)
-    {
+uint8_t lw(void* callback, uint32_t addr, uint32_t* dest){
+    if(addr == 0x024){
         *dest = (gettick() & 0xFFFFFFFF00000000) >> 32;
-        return true;
-    }
-    else if(addr == 0x028)
-    {
+        return 1;
+    }else if(addr == 0x028){
         *dest = gettick() & 0xFFFFFFFF;
-        return true;
+        return 1;
     }
-
-    *dest = (uint32_t)hb_regs->regs[addr >> 2];
-    return true;
+    *dest = (uint32_t)hb_sd_regs.regs[addr>>2];
+    return 1;
+}
+uint8_t ld(void* callback, uint32_t addr, uint64_t* dest){
+    *dest = (uint64_t)hb_sd_regs.regs[addr>>2];
+    return 1;
+}
+uint8_t sb(void* callback, uint32_t addr, uint8_t* src){
+    hb_sd_regs.regs[addr>>2] = *src;
+    return 1;
+}
+uint8_t sh(void* callback, uint32_t addr, uint16_t* src){
+    hb_sd_regs.regs[addr>>2] = *src;
+    return 1;
 }
 
-bool ld(hb_sd_regs_t *hb_regs, uint32_t addr, uint64_t *dest)
-{
-    addr -= ADDR_OFFSET;
-    *dest = (uint64_t)homeboy_obj->regs[addr >> 2];
-    return true;
-}
-
-bool sb(hb_sd_regs_t *hb_regs, uint32_t addr, uint8_t *src)
-{
-    addr -= ADDR_OFFSET;
-    hb_regs->regs[addr >> 2] = *src;
-
-    return true;
-}
-
-bool sh(hb_sd_regs_t *hb_regs, uint32_t addr, uint16_t *src)
-{
-    addr -= ADDR_OFFSET;
-    hb_regs->regs[addr >> 2] = *src;
-
-    return true;
-}
-
-bool sw(hb_sd_regs_t *hb_regs, uint32_t addr, uint32_t *src)
-{
-    addr -= ADDR_OFFSET;
-    
-    homeboy_obj->regs[addr >> 2] = *src;
-    
-    if(addr == 0x08) 
-    {
+uint8_t sw(void* callback, uint32_t addr, uint32_t* src){
+    hb_sd_regs.regs[addr>>2] = *src;
+    if(addr==0x08){
         do_write();
     }
-    else if(addr == 0x0C)
-    {
+    if(addr == 0x0C){
         do_read();
     }
-    else if(addr == 0x14) 
-    {
+    if(addr==0x14){
         do_status_update();
     }
-
-    return true;
+    return 1;
 }
-
-bool sd(hb_sd_regs_t *hb_regs, uint32_t addr, uint64_t *src)
-{
-    addr -= ADDR_OFFSET;
+uint8_t sd(void* callback, uint32_t addr, uint64_t* src){
     uint32_t *src32 = (uint32_t*)src;
-    hb_regs->regs[addr >> 2] = src32[0];
-    hb_regs->regs[(addr >> 2) + 1] = src32[1];
-
-    return true;
+    hb_sd_regs.regs[addr>>2] = src32[0];
+    hb_sd_regs.regs[(addr>>2) + 1] = src32[1];
+    return 1;
 }
 
-int homeboy_event(void *regs, int event, void *arg)
-{
-    if(event == 0x1002)
-    {
-        cpuSetDevicePut(gSystem->cpu, arg, sb, sh, sw, sd);
-        cpuSetDeviceGet(gSystem->cpu, arg, lb, lh, lw, ld);
+uint8_t unk_0x2C_(void* callback, uint32_t addr, void* unk){
+    return 1;
+}
+
+ENTRY bool _start(void **dest, size_t size){
+
+    if(hb_hid<0){
+        hb_hid = ios_create_heap((void*)ios_heap_addr,HB_HEAPSIZE);
     }
-}
+    if(hb_hid>=0){
+        hb_sd_regs.key = 0x1234;
+    }
 
-void homeboy_init(void)
-{
-    xlObjectMake((void**)&homeboy_obj, NULL, &homeboy_class);
-    cpuMapObject(gSystem->cpu, homeboy_obj, 0x08050000, 0x08057FFF, 0);
+    fs_init();
+
+#if VC_VERSION == NACE
+    n64_cpu->mem_index[(hb_mmreg >> 12) & 0xFFFFF] = 0x70;
+#else
+    n64_cpu->mem_index[(hb_mmreg >> 16) & 0xFFFF] = 0x70;
+#endif
+    
+    n64_cpu->memory_domain[0x70] = &memory;
+
+    bool ret = ramSetSize(dest,N64_DRAM_SIZE);
+    if(ret){
+        n64_dram = dest[1];
+    }
+
+    sprintf(dram_fn,"/title/00010001/%8x/data/dram_save",title_id);
+    
+    // Check if a dram restore needs to be done. 
+    int fd = fs_open(dram_fn,1);
+    if(fd>=0){
+        uint32_t dram_params[2];
+        fs_read(fd,dram_params,sizeof(dram_params));
+        fs_read(fd,(char*)n64_dram + dram_params[0], dram_params[1]);
+        fs_close(fd);
+        fs_delete(dram_fn);
+        hb_sd_regs.dram_restore_key = 0x6864;
+    }
+
+    return ret;
 }
